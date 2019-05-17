c6e737e0ff2f78fa0af56ed9a879980d74d8858c
Jose-Armando-Juarez <josejuarez.ca@gmail.com>
2019-05-17 16:30:30 -0400

    functionality for site

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function menu(){
+
+  const menu = document.querySelector(".navigation");
+      
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+
+}
